f6420883834990f2d434df0ce7a457e720481ea9
tonio789 <tonio.alvarez.rojas@gmail.com>
2023-08-17 00:10:01 -0600

    landing

--- WPFTaskPro/WPFTaskPro_WS/Landing.xaml.cs
+++ WPFTaskPro/WPFTaskPro_WS/Landing.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,13 +25,69 @@ namespace WPFTaskPro_WS
         {
             InitializeComponent();
             tbuser.Text = u.username;
+            listselected = null;
+            this.user = u;
+            Refresh();
         }
+        user user;
+        list listselected = new list();
+
+
+        private void Refresh()
+        {
+
+
+            DGListas.ItemsSource = new ConvertRow().listtolist(sw.listReadByUserID(this.user.id));
+        }
+
+
+
         SWRef.WebService1SoapClient sw = new SWRef.WebService1SoapClient();
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btn_newlist(object sender, RoutedEventArgs e)
         {
             string newlist = tb_newlist.Text;
+            string commentlist = sw.listCreate(newlist);
 
-            sw.listCreate(newlist);
+            MessageBox.Show(commentlist);
+
+            if (commentlist == "Lista agregada correctamente")
+            {
+                int idlastlist = sw.listReadAll().Tables[0].Rows.Count - 1;
+                DataRow lastlist = sw.listReadAll().Tables[0].Rows[idlastlist];
+                int listid = int.Parse(lastlist["id"].ToString());
+
+                sw.listAccessCreateUpdate(this.user.id, listid, "OWNER");
+            }
+
+            tb_newlist.Text = "";
+            Refresh();
         }
+
+        private void btn_newtask(object sender, RoutedEventArgs e)
+        {
+            string newtarea = tb_newtask.Text;
+            string commenttask = sw.taskCreate(newtarea, "","NOT STARTED", "N","N","","","MEDIUM", 1/*listselected.id*/);
+
+            MessageBox.Show(commenttask);
+
+            tb_newtask.Text = "";
+        }
+
+        private void btn_eliminarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void btn_list_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        
+
+
+
+
     }
 }

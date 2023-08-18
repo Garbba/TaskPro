e728a7e4562b7004d3330f4a937d9ebbc8820c1e
tonio789 <tonio.alvarez.rojas@gmail.com>
2023-08-17 21:55:34 -0600

    listaccess_username

---
+++ WPFTaskPro/WPFTaskPro_WS/O/listaccess_username.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTaskPro_WS.O
+{
+    public class listaccess_username
+    {
+        public string user { get; set; }
+    }
+}
